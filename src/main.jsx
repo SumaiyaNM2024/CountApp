6f009a3d0518ed6ac9ea12eb105d90443bdc2a7d
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2024-09-26 16:04:54 +0530

    props updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
 // import UseEffect from './UseEffect.jsx'
-import Rendering from './Rendering.jsx'
+// import Rendering from './Rendering.jsx'
+import Greeting from './Greeting.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <UseEffect/> */}
-    <Rendering />
+    {/* <Rendering /> */}
+    <Greeting name = "Saas "/>
   </StrictMode>,
 )
